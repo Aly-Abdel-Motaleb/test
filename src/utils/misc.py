1bf00ca196c6d2d59c4bb0d6d7f98eace4472437
Aly Abdelmotaleb <mostafa.elsayed.2002@gmail.com>
2025-06-23 02:53:32 +0300

    fixed

--- src/utils/misc.py
+++ src/utils/misc.py
@@ -5,6 +5,8 @@ import torch
 
 EPSILON = 1E-10
 
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, PROJECT_ROOT)
 
 def init_env(cfg):
     np.random.seed(cfg.seed)
